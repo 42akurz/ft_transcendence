e780d76828017910c5f3420f1f2ba7996f45bd2e
Alexander Kurz <akurz@1-E-3.42heilbronn.de>
2022-11-11 11:44:49 +0100

    add removeEventlistener in GameRoomView
    - fixes double emits and lag during game

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../users/users.entity'
 import { Score } from '../scoreboard/score.entity'
 import { GameGateway } from './game.gateway';
-import { UsersService } from '../users/users.service';
-import { ScoreService } from '../scoreboard/score.service';
 import { GameService } from './game.service';
-import DatabaseFilesService from 'src/files/databaseFiles.service';
-import DatabaseFile from 'src/files/databaseFile.entity';
-import DatabaseFilesController from 'src/files/databaseFiles.controller';
 import { UsersModule } from 'src/users/users.module';
 import { ScoreModule } from 'src/scoreboard/score.module';
+import { GameController } from './game.controller';
 
 @Module({
 	imports: [
@@ -20,5 +15,6 @@ import { ScoreModule } from 'src/scoreboard/score.module';
 	],
 	providers: [GameService, GameGateway],
 	exports: [GameService],
+	controllers: [GameController]
 })
 export class GameModule {}
