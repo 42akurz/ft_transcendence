9d5b29d5f73e1c942d5523ed729fd6e4672ea85f
Alexander Kurz <akurz@1-E-7.42heilbronn.de>
2022-11-17 16:21:48 +0100

    fix styling of GamePauseMenu and GameResult

--- frontend/src/components/GamePauseMenu.vue
+++ frontend/src/components/GamePauseMenu.vue
@@ -20,38 +20,36 @@
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
-
 		background-color: var(--blue-light);
 		box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-
 		gap: 30px;
-		border-radius: 42px;
-
 		width: 370px;
 		height: 370px;
 	}
 
 	strong {
 		color: white;
-		font-size: 63px;
+		font-size: 35px;
+		font-weight: bold;
 	}
 
 	button {
 		width: 168px;
 		height: 42px;
-		cursor: pointer;
 		margin: 0 10px;
-
-		border: 2px solid var(--blue-dark);
 		background-color: var(--orange);
-		color: var(--blue-dark);
-		border-radius: 25px;
-
+		border: 2px solid black;
+		color: black;
+		border-radius: 5px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 		font-size: 21px;
+		font-weight: bold;
+		cursor: pointer;
 	}
 </style>
