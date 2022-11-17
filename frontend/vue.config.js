e6742fe0ce93674998ea02aa903f4e04c46abf67
Felix Haring <fharing@1-D-26.42heilbronn.de>
2022-11-17 20:17:37 +0100

    change compose

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,17 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: { port: 8080 }
+  devServer: { port: 8080 , host: "0.0.0.0"}
 })
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   devServer: { webSocketServer: '169.254.128.215',port: 8080}
-// })
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   devServer: { server: '169.254.128.215', port: 8080}
-// })
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   devServer: { host: '169.254.128.215',port: 8080}
-// })
