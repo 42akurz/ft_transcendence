0950adc72710f1534803cc444e118bb74b5f13a2
Alexander Kurz <akurz@1-E-7.42heilbronn.de>
2022-11-17 11:59:54 +0100

    add 42 Logo to login button

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="login-wrapper">
-		<button @click="login">Login</button>
+		<button @click="login">
+			<img src="@/assets/42-logo-white.svg">
+			<strong>Login</strong>
+		</button>
 	</div>
 </template>
 
@@ -32,9 +35,9 @@
 	
 	button {
 		width: 200px;
-		height: 60px;
-		background-color: var(--blue-light);
-		border: 2px solid black;
+		height: 70px;
+		background-color: var(--blue-dark);
+		border: 2px solid var(--blue-dark);
 		color: white;
 		border-radius: 5px;
 		display: flex;
@@ -43,5 +46,20 @@
 		font-size: 12px;
 		font-weight: bold;
 		cursor: pointer;
+	}
+
+	button:hover {
+		background-color: var(--blue-light);
+	}
+
+	img {
+		height: 35px;
+		margin-right: 5px;
+	}
+
+	strong {
+		font-size: 20px;
+		margin-left: 5px;
+		font-weight: bold;
 	}
 </style>
