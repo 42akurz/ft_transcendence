d824e9f6fa966ffd695752d7e40516f0fa00c3f5
Alexander Kurz <akurz@1-E-7.42heilbronn.de>
2022-11-17 11:04:13 +0100

    fix game running in background issues
    - when i quit during countdown the gameInterval was cleared
      before it even started
    - games where always runnign in background when somebody quit
      during countdown
    - fix was to set GameData before startGameCountdown
    - if you exitGame now the gameData will be deleted
    - gameLoop will never execute because GameData is unset

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-}
-)
+  devServer: { port: 8080 }
+})
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   devServer: { webSocketServer: '169.254.128.215',port: 8080}
+// })
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   devServer: { server: '169.254.128.215', port: 8080}
+// })
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   devServer: { host: '169.254.128.215',port: 8080}
+// })
